82f02f9d32d10ecf2476326e0ab0e496211fcbb0
idiottrytocode <73723669+idiottrytocode@users.noreply.github.com>
2020-11-07 21:21:16 -0500

    Add files via upload

---
+++ database.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase 
+{
+	private	String query; 
+	public Datebase(String query){
+		this.query = query; 	
+	}
+	
+	// 
+	// I have no idea what do you want the database to do? 
+	// do you want to create a database from screthc?!?! 	
+}
